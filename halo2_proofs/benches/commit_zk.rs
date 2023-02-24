1547fa174faf4709bd5a5cd8629b7ecbd6328ce2
CPerezz <c.perezbaro@gmail.com>
2023-02-24 17:13:14 +0100

    chore: add benches for commit_zk serial vs par

---
+++ halo2_proofs/benches/commit_zk.rs
@@ -0,0 +1,69 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use group::ff::Field;
+use halo2_proofs::poly::{LagrangeCoeff, Polynomial};
+use halo2_proofs::*;
+use halo2curves::pasta::pallas::Scalar;
+use rand_chacha::rand_core::RngCore;
+use rand_chacha::ChaCha20Rng;
+use rand_core::SeedableRng;
+use rayon::{
+    current_num_threads,
+    prelude::{IntoParallelRefIterator, ParallelIterator},
+};
+
+fn rand_poly_serial(mut rng: ChaCha20Rng, domain: usize) -> Polynomial<Scalar, LagrangeCoeff> {
+    // Sample a random polynomial of degree n - 1
+    let mut random_poly = vec![Scalar::zero(); 1 << domain];
+    for coeff in random_poly.iter_mut() {
+        *coeff = Scalar::random(&mut rng);
+    }
+    assert_eq!(random_poly.len(), 1 << domain);
+    Polynomial::from_evals(random_poly)
+}
+
+fn rand_poly_par(mut rng: ChaCha20Rng, domain: usize) -> Polynomial<Scalar, LagrangeCoeff> {
+    // Sample a random polynomial of degree n - 1
+    let n_threads = current_num_threads();
+    let needed_scalars = (1 << domain) / n_threads;
+
+    let thread_seeds: Vec<ChaCha20Rng> = (0..n_threads)
+        .into_iter()
+        .map(|_| {
+            let mut seed = [0u8; 32];
+            rng.fill_bytes(&mut seed);
+            ChaCha20Rng::from_seed(seed)
+        })
+        .collect();
+
+    let rand_vec: Vec<Scalar> = thread_seeds
+        .par_iter()
+        .flat_map(|rng| vec![Scalar::random(rng.to_owned()); needed_scalars])
+        .collect();
+
+    assert_eq!(rand_vec.len(), 1 << domain);
+    Polynomial::from_evals(rand_vec)
+}
+
+fn bench_commit(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Blinder_poly");
+    let rand = ChaCha20Rng::from_seed([1u8; 32]);
+    for i in [
+        18usize, 19usize, 20usize, 21usize, 22usize, 23usize, 24usize, 25usize,
+    ]
+    .iter()
+    {
+        group.bench_with_input(BenchmarkId::new("serial", i), i, |b, i| {
+            b.iter(|| rand_poly_serial(rand.clone(), *i))
+        });
+        group.bench_with_input(BenchmarkId::new("parallel", i), i, |b, i| {
+            b.iter(|| rand_poly_par(rand.clone(), *i))
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_commit);
+criterion_main!(benches);
