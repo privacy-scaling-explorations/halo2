940aaa0591a082e96ed160d2dae50dd571d3a228
Chao Ma <mc8305@gmail.com>
2023-09-07 18:51:19 -0700

    add bench code for baseline msm

--- halo2_proofs/benches/arithmetic.rs
+++ halo2_proofs/benches/arithmetic.rs
@@ -1,36 +1,47 @@
 #[macro_use]
 extern crate criterion;
 
-use crate::arithmetic::small_multiexp;
+use crate::arithmetic::{best_multiexp, CurveAffine};
 use crate::halo2curves::pasta::{EqAffine, Fp};
-use group::ff::Field;
+use ff::PrimeField;
 use halo2_proofs::*;
 
 use halo2_proofs::poly::{commitment::ParamsProver, ipa::commitment::ParamsIPA};
 
-use criterion::{black_box, Criterion};
+use criterion::{black_box, BenchmarkId, Criterion};
 use rand_core::OsRng;
 
-fn criterion_benchmark(c: &mut Criterion) {
+pub fn generate_msm_inputs<C, F>(size: usize) -> (Vec<C>, Vec<F>)
+where
+    F: PrimeField,
+    C: CurveAffine<ScalarExt=F>,
+{
     let rng = OsRng;
+    let vec_len = 1 << size;
+    let scalar_vec = (0..vec_len)
+        .map(|_| F::random(rng))
+        .collect();
+    let params: ParamsIPA<C> = ParamsIPA::new(size as u32);
+    let point_vec = params.get_g().to_vec();
+    (
+        point_vec,
+        scalar_vec,
+    )
+}
 
-    // small multiexp
-    {
-        let params: ParamsIPA<EqAffine> = ParamsIPA::new(5);
-        let g = &mut params.get_g().to_vec();
-        let len = g.len() / 2;
-        let (g_lo, g_hi) = g.split_at_mut(len);
-
-        let coeff_1 = Fp::random(rng);
-        let coeff_2 = Fp::random(rng);
-
-        c.bench_function("double-and-add", |b| {
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("msm");
+    for size in 8..10 {
+        let (point_vec, scalar_vec) = generate_msm_inputs::<EqAffine, Fp>(size);
+        let point_vec = black_box(point_vec);
+        let scalar_vec = black_box(scalar_vec);
+        group.bench_with_input(BenchmarkId::new("Baseline", size), &size, |b, _size| {
             b.iter(|| {
-                for (g_lo, g_hi) in g_lo.iter().zip(g_hi.iter()) {
-                    small_multiexp(&[black_box(coeff_1), black_box(coeff_2)], &[*g_lo, *g_hi]);
-                }
+                let _ = best_multiexp(&scalar_vec[..], &point_vec[..]);
             })
         });
+        // add optimized function here
+        // group.bench_with_input(BenchmarId::new("Optimized, size) ....
     }
 }
 
