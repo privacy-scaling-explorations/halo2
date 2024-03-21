fabc3ea16eed485a4b570388f5e354798085647b
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-03-21 20:38:14 +0800

    ci: update `.github/scripts/wasm-target-test-build.sh`
    Co-authored-by: Eduard S. <eduardsanou@posteo.net>

--- .github/scripts/wasm-target-test-build.sh
+++ .github/scripts/wasm-target-test-build.sh
@@ -10,7 +10,7 @@ rustup target add wasm32-unknown-unknown wasm32-wasi
 cargo new foobar
 cd foobar
 cargo add --path "${GIT_ROOT}/halo2_proofs" --features batch,dev-graph,gadget-traces
-cargo add getrandom --features js
+cargo add getrandom --features js --target wasm32-unknown-unknown
 
 cargo build --release --target wasm32-unknown-unknown
 cargo build --release --target wasm32-wasi
