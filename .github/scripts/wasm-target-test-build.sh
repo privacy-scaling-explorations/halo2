9564e1319cd09642d5aa5fa1b2461a35cdba65fe
duguorong <80258679+duguorong009@users.noreply.github.com>
2024-03-21 16:12:53 +0800

    fix: correct the wasm target test build script

--- .github/scripts/wasm-target-test-build.sh
+++ .github/scripts/wasm-target-test-build.sh
@@ -2,16 +2,16 @@ GIT_ROOT=$(pwd)
 
 cd /tmp
 
-cp -R "${GIT_ROOT}" . || true
-cd halo2
+rustup default stable
 rustup target add wasm32-unknown-unknown wasm32-wasi
 
-cd halo2_proofs
+cargo new foobar
+cd foobar
+cargo add --path "${GIT_ROOT}/halo2_proofs" --features batch,dev-graph,gadget-traces
 cargo add getrandom --features js
-cd ../
 
-cargo build --release --no-default-features --features batch,dev-graph,gadget-traces --target wasm32-unknown-unknown
-cargo build --release --no-default-features --features batch,dev-graph,gadget-traces --target wasm32-wasi
+cargo build --release --target wasm32-unknown-unknown
+cargo build --release --target wasm32-wasi
 
 cd ../
-rm -rf halo2
+rm -rf foobar
