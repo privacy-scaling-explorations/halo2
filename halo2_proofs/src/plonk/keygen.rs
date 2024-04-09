456caa24538029be10478360089e0b4d8d2c71ab
duguorong <80258679+duguorong009@users.noreply.github.com>
2024-04-09 10:26:15 +0800

    chore: update the comments

--- halo2_proofs/src/plonk/keygen.rs
+++ halo2_proofs/src/plonk/keygen.rs
@@ -11,8 +11,8 @@ use halo2_middleware::ff::FromUniformBytes;
 /// Generate a `VerifyingKey` from an instance of `Circuit`.
 /// By default, selector compression is turned **ON**.
 ///
-/// **NOTE**: This `keygen_vk` is legacy one, assuming that `compress_selector: true`.
-/// Hence, this one MUST be paired with `keygen_pk` util.
+/// **NOTE**: This `keygen_vk` is legacy one, assuming that `compress_selector: true`.  
+/// Hence, it is HIGHLY recommended to pair this util with `keygen_pk`.  
 /// In addition, when using this for key generation, user MUST use `compress_selectors: true`.
 pub fn keygen_vk<'params, C, P, ConcreteCircuit>(
     params: &P,
@@ -31,8 +31,10 @@ where
 ///
 /// The selector compression optimization is turned on only if `compress_selectors` is `true`.
 ///
-/// **NOTE**: This `keygen_vk_custom` MUST share the same `compress_selectors` with `keygen_pk_custom`.
-/// Otherwise, the user would get unmatching pk/vk pair.
+/// **NOTE**: This `keygen_vk_custom` MUST share the same `compress_selectors` with
+/// `ProvingKey` generation process.
+/// Otherwise, the user could get unmatching pk/vk pair.  
+/// Hence, it is HIGHLY recommended to pair this util with `keygen_pk_custom`.
 pub fn keygen_vk_custom<'params, C, P, ConcreteCircuit>(
     params: &P,
     circuit: &ConcreteCircuit,
@@ -51,8 +53,8 @@ where
 /// Generate a `ProvingKey` from a `VerifyingKey` and an instance of `Circuit`.
 /// By default, selector compression is turned **ON**.
 ///
-/// **NOTE**: This `keygen_pk` is legacy one, assuming that `compress_selector: true`.
-/// Hence, this one MUST be paired with `keygen_vk` util.
+/// **NOTE**: This `keygen_pk` is legacy one, assuming that `compress_selector: true`.  
+/// Hence, it is HIGHLY recommended to pair this util with `keygen_vk`.  
 /// In addition, when using this for key generation, user MUST use `compress_selectors: true`.
 pub fn keygen_pk<'params, C, P, ConcreteCircuit>(
     params: &P,
@@ -71,8 +73,10 @@ where
 ///
 /// The selector compression optimization is turned on only if `compress_selectors` is `true`.
 ///
-/// **NOTE**: This `keygen_pk_custom` MUST share the same `compress_selectors` with `keygen_vk_custom`.
-/// Otherwise, the user would get unmatching pk/vk pair.
+/// **NOTE**: This `keygen_pk_custom` MUST share the same `compress_selectors` with
+/// `VerifyingKey` generation process.
+/// Otherwise, the user could get unmatching pk/vk pair.  
+/// Hence, it is HIGHLY recommended to pair this util with `keygen_vk_custom`.
 pub fn keygen_pk_custom<'params, C, P, ConcreteCircuit>(
     params: &P,
     vk: VerifyingKey<C>,
